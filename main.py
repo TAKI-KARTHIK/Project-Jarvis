f0cd706d8ddc9526ae30c03a88dfb3ce5ec93f23
K∆RTHIK <127835057+TAKI-KARTHIK@users.noreply.github.com>
2024-06-16 12:52:16 +0000

    pocketsphinx installed

--- main.py
+++ main.py
@@ -12,5 +12,19 @@ def speak(text):
 
 #Python's if __name__ == "__main__": construct enables a single Python file to not only support reusable code and functions, but also contain executable code that will not explicitly run when a module is imported.
 if __name__ == "__main__":
-    speak("Listerning......")
-    #
+    speak("Initializing......")
+    while True:
+        #Listerning for the wakeup
+        r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+# recognize speech using Sphinx
+try:
+    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+except sr.UnknownValueError:
+    print("Sphinx could not understand audio")
+except sr.RequestError as e:
+    print("Sphinx error; {0}".format(e)) 
+    
