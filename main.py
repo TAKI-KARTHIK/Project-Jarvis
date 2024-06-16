a25d7f29380126d39c22b3d969bdab3b1a7be7e0
KARTHIK <karthikiyer1123@gmail.com>
2024-06-16 19:26:04 +0530

    speech recognition and opening webapps done

--- main.py
+++ main.py
@@ -8,23 +8,40 @@ engine = pyttsx3.init()
 def speak(text):
     engine.say(text)
     engine.runAndWait()
-
+def process_Command(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://youtube.com")
+    elif "open instagram" in c.lower():
+        webbrowser.open("https://instagram.com")
+    pass
 
 #Python's if __name__ == "__main__": construct enables a single Python file to not only support reusable code and functions, but also contain executable code that will not explicitly run when a module is imported.
 if __name__ == "__main__":
     speak("Initializing......")
     while True:
-        #Listerning for the wakeup
+        #Listerning for the wakeup word
         r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
+        print("Recognizing...")    
+        try:
+            with sr.Microphone() as source:
+                print("Listerning..")
+                audio = r.listen(source) #You can even add timeout and phrase_time_limit parameter
+                word = r.recognize_google(audio)
+                if(word.lower() == "jarvis"):
+                   speak("Yes sir, How can i help you")
+                   #listerning for the userinput
+                   with sr.Microphone() as source:
+                       print("Jarvis Active..")
+                       audio = r.listen(source)
+                       command = r.recognize_google(audio)
 
-# recognize speech using Sphinx
-try:
-    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
-except sr.UnknownValueError:
-    print("Sphinx could not understand audio")
-except sr.RequestError as e:
-    print("Sphinx error; {0}".format(e)) 
+                       process_Command(command)
+
+
+        except sr.UnknownValueError:
+            print("Sphinx could not understand audio")
+        except sr.RequestError as e:
+            print("Sphinx error; {0}".format(e)) 
     
