95a9fdf8e19418f67e931bf76c18d328d57ac08d
KARTHIK <karthikiyer1123@gmail.com>
2024-06-16 18:16:31 +0530

    modules installed

--- main.py
+++ main.py
@@ -3,4 +3,14 @@ import webbrowser
 import pyttsx3
 
 recognizer = sr.Recognizer()
-ttsx = pyttsx3.init()
+engine = pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+#Python's if __name__ == "__main__": construct enables a single Python file to not only support reusable code and functions, but also contain executable code that will not explicitly run when a module is imported.
+if __name__ == "__main__":
+    speak("Listerning......")
+    #
